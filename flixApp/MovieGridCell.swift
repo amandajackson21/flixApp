c70978c8b56f6dacdd23e7e9f7e7500be9214e20
Amanda Jackson <Amanda@wireless-10-145-35-163.public.utexas.edu>
2019-02-16 14:03:15 -0600

    addition of part 2 to add second tab and access more detailed information

---
+++ flixApp/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixApp
+//
+//  Created by Amanda Jackson on 2/11/19.
+//  Copyright © 2019 Amanda Jackson. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
